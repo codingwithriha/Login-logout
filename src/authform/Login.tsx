18bad303dee1b5897e5c194d70e0aea2b67711bb
codingwithrih <rihashehzadi2003@gmail.com>
2024-10-16 20:59:46 +0500

    first commit

---
+++ src/authform/Login.tsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const auth = getAuth();
+    const navigate = useNavigate();
+    
+    const [authing, setAuthing] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const signInWithGoogle = async () => {
+        setAuthing(true);
+        
+        signInWithPopup(auth, new GoogleAuthProvider())
+            .then(response => {
+                console.log(response.user.uid);
+                navigate('/');
+            })
+            .catch(error => {
+                console.log(error);
+                setAuthing(false);
+            });
+    }
+
+    const signInWithEmail = async () => {
+        setAuthing(true);
+        setError('');
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(response => {
+                console.log(response.user.uid);
+                navigate('/');
+            })
+            .catch(error => {
+                console.log(error);
+                setError(error.message);
+                setAuthing(false);
+            });
+    }
+
+    return (
+        <div className='w-full h-screen flex'>
+            <div className='w-1/2 h-full flex flex-col bg-[url("./assets/login-banner.png")] items-center justify-center'>
+            </div>
+            <div className='w-1/2 h-full bg-[#081D5A] flex flex-col p-20 justify-center'>
+                <div className='w-full flex flex-col max-w-[450px] mx-auto'>
+                    <div className='w-full flex flex-col mb-10 text-white'>
+                        <h3 className='text-4xl font-bold mb-2'>Login</h3>
+                        <p className='text-lg mb-4'>Enter your username or email below to login to your account</p>
+                    </div>
+                    <div className='w-full flex flex-col mb-6'>
+                        <input
+                            type='email'
+                            placeholder='Email'
+                            className='w-full text-white py-2 mb-4 bg-transparent border-b border-gray-500 focus:outline-none focus:border-white'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)} />
+                        <input
+                            type='password'
+                            placeholder='Password'
+                            className='w-full text-white py-2 mb-4 bg-transparent border-b border-gray-500 focus:outline-none focus:border-white'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div className='w-full flex flex-col mb-4'>
+                        <button
+                            className='w-full bg-transparent border border-white text-white my-2 font-semibold rounded-md p-4 text-center flex items-center justify-center cursor-pointer'
+                            onClick={signInWithEmail}
+                            disabled={authing}>
+                            Log In
+                        </button>
+                    </div>
+                    {error && <div className='text-red-500 mb-4'>{error}</div>}
+                    <div className='w-full flex items-center justify-center relative py-4'>
+                        <div className='w-full h-[1px] bg-gray-500'></div>
+                        <p className='text-lg absolute text-gray-500 bg-[#081D5A] px-2'>OR</p>
+                    </div>
+                    <button
+                        className='w-full bg-white text-[#081D5A] font-semibold rounded-md p-4 text-center flex items-center justify-center cursor-pointer mt-7'
+                        onClick={signInWithGoogle}
+                        disabled={authing}>
+                        Log In With Google
+                    </button>
+                </div>
+                <div className='w-full flex items-center justify-center mt-10'>
+                    <p className='text-sm font-normal text-gray-400'>Don't have an account? <span className='font-semibold text-white cursor-pointer underline'><a href='/signup'>Sign Up</a></span></p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import App from './App.tsx'
+import Login from './authform/Login.tsx'
+import Signup from './authform/Signup.tsx'
+import AuthRoute from './authform/AuthRoute.tsx'
+import './index.css'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-5rmyUJ2pUv1BGrUXSHZvhOJKBGaTqRU",
+  authDomain: "auth-17d07.firebaseapp.com",
+  projectId: "auth-17d07",
+  storageBucket: "auth-17d07.appspot.com",
+  messagingSenderId: "500549119468",
+  appId: "1:500549119468:web:ecf7cbb4a9c83dcc92e4c9"
+};
+
+initializeApp(firebaseConfig);
+
+ReactDom.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path='/' element={<AuthRoute><App /></AuthRoute>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)
